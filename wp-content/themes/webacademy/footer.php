d33538ca96721993bba6a193d734250c1ca52199
Sumit_Timalsina <sumi0080@edu.easj.dk>
2022-06-21 21:44:37 +0200

    tekst

--- wp-content/themes/webacademy/footer.php
+++ wp-content/themes/webacademy/footer.php
@@ -5,7 +5,7 @@
             <h1 class="school-logo-text school-logo-text--alt-color">
               <a href="<?php echo site_url()?>"><strong>Web</strong>academy</a>
             </h1>
-            <p><a class="site-footer__link" href="">555.555.5555</a></p>
+            <p><a class="site-footer__link" href="">(+45)91765430</a></p>
           </div>
 
           <div class="site-footer__col-two-three-group">
--- wp-content/themes/webacademy/front-page.php
+++ wp-content/themes/webacademy/front-page.php
@@ -1,11 +1,11 @@
 <?php get_header(); ?>
 
   <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>);"></div>
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/building.jpg') ?>);"></div>
       <div class="page-banner__content container t-center c-white">
         <h1 class="headline headline--large">Hello!</h1>
-        <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
-        <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
+        <h2 class="headline headline--medium">Welcome to Webacademy</h2>
+        <h3 class="headline headline--small">Get started by checking out our majors by clicking the button below</h3>
         <a href="<?php echo get_post_type_archive_link('program'); ?>" class="btn btn--large btn--blue">Find Your Major</a>
       </div>
     </div>
@@ -80,30 +80,30 @@
     <div class="hero-slider">
       <div data-glide-el="track" class="glide__track">
         <div class="glide__slides">
-          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bus.jpg') ?>);">
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/canteen.jpg') ?>);">
             <div class="hero-slider__interior container">
               <div class="hero-slider__overlay">
-                <h2 class="headline headline--medium t-center">Free Transportation</h2>
-                <p class="t-center">All students have free unlimited bus fare.</p>
+                <h2 class="headline headline--medium t-center">Healthy canteen food</h2>
+                <p class="t-center">All students have access to healthy food in our canteen</p>
                 <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
               </div>
             </div>
           </div>
-          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/apples.jpg') ?>);">
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/friday.jpg') ?>);">
             <div class="hero-slider__interior container">
               <div class="hero-slider__overlay">
-                <h2 class="headline headline--medium t-center">An Apple a Day</h2>
-                <p class="t-center">Our dentistry program recommends eating apples.</p>
+                <h2 class="headline headline--medium t-center">Friday bars </h2>
+                <p class="t-center">Join our friday bars for games, fun and booze.</p>
                 <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
               </div>
             </div>
           </div>
-          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bread.jpg') ?>);">
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/massage.jpg') ?>);">
             <div class="hero-slider__interior container">
               <div class="hero-slider__overlay">
-                <h2 class="headline headline--medium t-center">Free Food</h2>
-                <p class="t-center">Webacademy offers lunch plans for those in need.</p>
-                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+                <h2 class="headline headline--medium t-center">Free massage </h2>
+                <p class="t-center">Let all that stress off and relax our free massage.</p>
+                <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('p'); ?>" class="btn btn--blue">Learn more</a></p>
               </div>
             </div>
           </div>
